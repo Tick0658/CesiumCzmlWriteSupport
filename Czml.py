6eb27c1315d4121b99d1a91ccbf3ea2ccbed9b26
keisuke <ronkun0658@yahoo.co.jp>
2024-03-24 16:54:36 +0900

    20240324Commit

--- Czml.py
+++ Czml.py
@@ -2,6 +2,18 @@ import pandas as pd
 
 
 class Czml:
+    
+    class Position:
+        # 配列に 3つの要素がある場合、値は定数です。
+        # 4つ以上の要素がある場合、それらは [Time, X, Y, Z, Time, X, Y, Z, ...] のように配置された時間タグ付きサンプルです。
+        # Time は ISO 8601 の日付と時刻の文字列または秒です。
+        cartesian = {"cartesian": []}
+        # 3 次元デカルト値 [X, Y, Z] として指定される位置 (referenceFrame を基準としたメートル単位)。
+        cartographicRadians = {"cartographicRadians": []}
+        # WGS84 座標 [経度、緯度、高さ] で指定された位置。経度と緯度はラジアン単位、高さはメートル単位
+        cartographicDegrees = {"cartographicDegrees": []}
+        # WGS84 座標 [経度、緯度、高さ] で指定された位置。経度と緯度は度数単位、高さはメートル単位
+
     class Path:
         path = {
             "path": {
@@ -103,16 +115,118 @@ class Czml:
             }
         }
 
-    class Position:
-        #配列に 3つの要素がある場合、値は定数です。 
-        #4つ以上の要素がある場合、それらは [Time, X, Y, Z, Time, X, Y, Z, ...] のように配置された時間タグ付きサンプルです。
-        # Time は ISO 8601 の日付と時刻の文字列または秒です。
-        cartesian = {"cartesian": []}
-        #3 次元デカルト値 [X, Y, Z] として指定される位置 (referenceFrame を基準としたメートル単位)。
-        cartographicRadians ={"cartographicRadians":[]}
-        #WGS84 座標 [経度、緯度、高さ] で指定された位置。経度と緯度はラジアン単位、高さはメートル単位
-        cartographicDegrees={"cartographicDegrees":{}}
-        #WGS84 座標 [経度、緯度、高さ] で指定された位置。経度と緯度は度数単位、高さはメートル単位
+    
+    class Properties:
+        properties = {
+            "properties": {
+                "id": None,
+                # このパケットに記述されているオブジェクトのID
+                # IDはGUIDである必要はないが、CZMLソースや同じスコープにロードされた他の
+                # CZMLソースの中で一つのオブジェクトを一意に識別する必要がある。
+                "nama": None,
+                # オブジェクトの名前。一意である必要はなく、ユーザーが使用するためのものです。
+                "parent": None,
+                # 親オブジェクトがある場合は、そのID。
+                "description": None,
+                # オブジェクトのHTML記述。
+                "clock": {},
+                # class clockを参照
+                # データセット全体の時計設定。ドキュメントオブジェクトでのみ有効。
+                "version": None,
+                # 書き込まれるCZMLのバージョン
+                "availability": "0000-00-00T00:00:00Z/9999-12-31T24:00:00Z",
+                # オブジェクトのデータが利用可能な時間間隔のセット
+                # このプロパティには、1つの時間間隔を表す文字列を指定することも、時間間隔を表す文字列の配列を指定することもできる
+                "properties": {},
+                # class CustomPropertyを参照
+                # このオブジェクトのカスタムプロパティのセット。
+                # この型はキーと値のマッピングを表し、値はCustomProperty型である。
+                "position": {},
+                # class Positionを参照
+                # ワールド内のオブジェクトの位置。この位置は直接的な視覚的表現はないが、
+                # ビルボードやラベルなど、オブジェクトに付属するグラフィカルなアイテムの位置を特定するために使用される。
+                "orientation": {},
+                # class Orientationを参照
+                # ワールド内のオブジェクトの向き。方位は直接視覚的な表現を持たないが、モデル、円錐、ピラミッド、
+                # その他オブジェクトに付属するグラフィカルアイテムを方向付けるために使用される。
+                "viewFrom": {},
+                #class ViewFrom参照
+                #このオブジェクトを表示するときに推奨されるカメラの位置。このプロパティは、オブジェクトの位置を基準とした東 (x)、北 (y)、上 (z) 参照フレーム内のデカルト位置として指定されます。
+                "billboard": {},
+                #class Billboard参照
+                #ビルボード、またはビューポートに位置合わせされたイメージ。マーカーとも呼ばれます。看板は敷地内のシーンに設置されていますposition。
+                "box": {},
+                #class Box参照
+                #閉じた直方体であるボックス。ボックスの位置と方向は、positionと のorientationプロパティを使用して設定されます。
+                "corridor": {},
+                #class Corridor参照
+                #コリドー。中心線と幅によって定義される形状です。
+                "cylinder": {},
+                #class Cylinder参照
+               # 長さ、上部半径、下部半径によって定義される円柱、円錐台、または円錐。円柱の位置と方向は、positionと のorientationプロパティを使用して設定されます。
+                "ellipse": {},
+                #class Ellipse参照
+                #地球の表面上の閉じた曲線である楕円。楕円はpositionプロパティを使用して配置されます。
+                "ellipsoid": {},
+                #class Ellipsoid参照
+                #楕円体。楕円の 3 次元類似物である閉じた二次曲面です。
+                # 楕円体の位置と方向は、positionおよびorientationプロパティを使用して設定されます。
+                "label": {},
+                #class label参照
+                #テキストの文字列。ラベルはプロパティによってシーン内に配置されますposition。
+                "model": {},
+                #class Model参照
+                #モデルの位置と方向は、positionと のorientationプロパティを使用して設定されます。
+                "path": {},
+                #class Path参照
+                #時間の経過に伴うオブジェクトの動きによって定義されるポリラインです。
+                # パスの可能な頂点はpositionプロパティによって指定されます。
+                "point": {},
+                #class Point参照
+                #点、またはビューポートに位置合わせされた円。
+                # ポイントはpositionプロパティによってシーン内に配置されます。
+                "polygon": {},
+                #class Polygon参照
+                #地球の表面上の閉じた図形である多角形。
+                "polyline": {},
+                #class Polyline参照
+                #ポリライン。複数のセグメントで構成されるシーン内の線です。
+                "polylineVolume": {},
+                #class PolylineVolume参照
+                #ボリュームのあるポリライン。
+                # ポリラインに沿って押し出された 2D 形状として定義されます。
+                "rectangle": {},
+                #class Rectangle参照
+                #地図作成用の長方形。地球の曲率に適合し、地表に沿ってまたは高度に配置できます。
+                "tileset": {},
+                #class Tileset参照
+                #3D タイルのタイルセット。
+                "wall": {},
+                # class Wall参照
+                # 地球の曲率に適合し、地表に沿ってまたは高度に配置できる 2 次元の壁。
+                "agi_conicSensor": {},
+                # class Agi_conicSensor参照
+                # 楕円体、つまり地球のオクルージョンを考慮した円錐形のセンサー ボリューム。
+                # センサーの位置と方向は、positionと のorientationプロパティを使用して設定されます。
+                "agi_customPatternSensor": {},
+                #class Agi_customPatternSensor参照
+                # 楕円体、つまり地球のオクルージョンを考慮したカスタム センサー ボリューム。
+                # センサーの位置と方向は、positionと のorientationプロパティを使用して設定されます。
+                "agi_rectangularSensor": {},
+                #class Agi_rectangularSensor参照
+                # 楕円体、つまり地球のオクルージョンを考慮した四角錐のセンサー ボリューム。
+                # センサーの位置と方向は、positionと のorientationプロパティを使用して設定されます。
+                "agi_fan": {},
+                #class Agi_fan参照
+                # 点または頂点から始まり、頂点から指定された方向のリストに広がるファンを定義します。
+                # 方向の各ペアは、指定された半径まで広がるファンの面を形成します。
+                # ファンは、positionと のorientationプロパティを使用して位置と方向を設定します。
+                "agi_vector": {},
+                #class Agi_vector参照
+                # positionプロパティを起点とし、指定された方向に指定された長さだけ伸びるグラフィック ベクトルを定義します。
+                # ベクトルはpositionプロパティを使用して配置されます。
+            }
+        }
 
 
 def Czmlsuppoter():
